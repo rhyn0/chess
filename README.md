8638b697eb96cbb5fce7f10d475e66126f5707b6
Ryan <ryan.ozawa21@gmail.com>
2021-03-26 17:12:27 -0700

    add starting GUI files, noted in README

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 #### Created by: Ryan Ozawa and Jacob Gold
 
 Hopefully this thing works.
+
+Using QT for GUI, and vcpkg.
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <QApplication>
 
 using namespace std;
 
 int main(){
-    std :: cout << "Hello World";
+    std :: cout << "Hello World" << qVersion() << endl;
     return 0;
 }
