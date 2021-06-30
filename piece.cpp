d6f1cba0690dc275a69d1519b2f8f0fb4e9add88
Ryan Ozawa <ryan.ozawa21@gmail.com>
2021-06-30 16:30:26 -0700

    distinguished inheritance, what is PositionScore

--- piece.cpp
+++ piece.cpp
@@ -1,89 +1,83 @@
 #include "piece.h";
 
-Piece::Piece(Color color) {
-    Piece::color = color;
+using namespace std;
 
-}
-
-std::vector<std::tuple<char, char>> Piece::getMoveList(Piece* squares[8][8], char x, char y) {
+vector<tuple<char, char>> Piece::getMoveList(Piece *squares[8][8], char x, char y)
+{
     Piece::moveList.clear();
     return Piece::moveList;
 }
 
-char Piece::getSymbol() {
-	return Piece::symbol;
+char Piece::getSymbol()
+{
+    return Piece::symbol;
 }
 
-int Piece::getValue() {
-	return Piece::value;
+int Piece::getValue()
+{
+    return Piece::value * (Piece::color == Color::White ? 1 : -1);
 }
 
-PieceType Piece::getPieceType() {
-	return Piece::pieceType;
+PieceType Piece::getPieceType()
+{
+    return Piece::pieceType;
 }
 
-int Piece::getPositionalScore(char x, char y) {
-	return 0;
+int Piece::getPositionalScore(char x, char y)
+{
+    return 0;
 }
 
-Pawn::Pawn(Color color) : Piece(color) {
+Pawn::Pawn(Color color)
+{
     Piece::color = color;
 }
 
-std::vector<std::tuple<char, char>> Pawn::getMoveList(Piece* squares[8][8], char x, char y) {
-	Piece::moveList.clear();
+std::vector<std::tuple<char, char>> Pawn::getMoveList(Piece *squares[8][8], char x, char y)
+{
+    Piece::moveList.clear();
 
-    if(Pawn::color == Color::White) {
-        
-        if(squares[x][y+1] == nullptr) {
-            Piece::moveList.push_back(std::tuple<char, char>(x, y+1)); //move pawn forward one square
+    if (Pawn::color == Color::White)
+    {
+
+        if (squares[x][y + 1] == nullptr)
+        {
+            Piece::moveList.push_back(std::tuple<char, char>(x, y + 1)); //move pawn forward one square
         }
 
-        if(y == 1) { //pawn has not moved yet
-            Piece::moveList.push_back(std::tuple<char, char>(x, y+2)); //move pawn forward two squares
+        if (y == 1)
+        {                                                                //pawn has not moved yet
+            Piece::moveList.push_back(std::tuple<char, char>(x, y + 2)); //move pawn forward two squares
         }
 
-        if((x < 7) && (squares[x+1][y+1] != nullptr) && (squares[x+1][y+1]->color != Pawn::color)) {
-            Piece::moveList.push_back(std::tuple<char, char>(x+1, y+2)); //take piece on right diagonal of pawn
+        if ((x < 7) && (squares[x + 1][y + 1] != nullptr) && (squares[x + 1][y + 1]->color != Pawn::color))
+        {
+            Piece::moveList.push_back(std::tuple<char, char>(x + 1, y + 2)); //take piece on right diagonal of pawn
         }
 
-        if((x > 0) && (squares[x-1][y+1] != nullptr) && (squares[x-1][y+1]->color != Pawn::color)) {
-            Piece::moveList.push_back(std::tuple<char, char>(x-1, y+2)); //take piece on left diagonal of pawn
+        if ((x > 0) && (squares[x - 1][y + 1] != nullptr) && (squares[x - 1][y + 1]->color != Pawn::color))
+        {
+            Piece::moveList.push_back(std::tuple<char, char>(x - 1, y + 2)); //take piece on left diagonal of pawn
         }
 
-        if(y == 4) {
-            
+        if (y == 4)
+        {
         }
     }
 
-    else {
-
+    else
+    {
     }
-
 }
 
-char Pawn::getSymbol() {
-	return Pawn::symbol;
-}
-
-int Pawn::getValue() {
-	if (Pawn::color == Color::White) {
-		return Pawn::value;
-	}
-	else {
-		return -Pawn::value;
-	}
-}
-
-PieceType Pawn::getPieceType() {
-	return Pawn::pieceType;
-}
-
-int Pawn::getPositionalScore(char x, char y) {
-	if (Pawn::color == Color::White) {
-		return Pawn::scoreBoard[7 - y][x];
-	}
-	else {
-		return -Pawn::scoreBoard[y][x];
-	}
+int Pawn::getPositionalScore(char x, char y)
+{
+    if (Pawn::color == Color::White)
+    {
+        return Pawn::scoreBoard[7 - y][x];
+    }
+    else
+    {
+        return -Pawn::scoreBoard[y][x];
+    }
 }
--- piece.h
+++ piece.h
@@ -1,104 +1,132 @@
 #include <tuple>
 #include <vector>
 
-enum class Color {None, White, Black};
-enum class PieceType {None, Pawn, Knight, Bishop, Rook, Queen, King};
-
-class Piece{
-    private:
-        static const char symbol = ' ';
-        static const int value = 0;
-        static const PieceType pieceType = PieceType::None;
-    public:
-        Color color;
-        virtual char getSymbol();
-	    virtual int getValue();
-	    virtual PieceType getPieceType();
-	    virtual int getPositionalScore(char x, char y);
-        std::vector<std::tuple<char, char>> moveList;
-        virtual std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y);
+enum class Color
+{
+    None,
+    White,
+    Black
+};
+enum class PieceType
+{
+    None,
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King
 };
 
-class Pawn : public Piece {
-    private: 
-        static const char symbol = 'p';
-        static const int value = 1;
-        static const PieceType pieceType = PieceType::Pawn;
-    public:
-        Pawn(Color color);
-        char getSymbol() override;
-	    int getValue() override;
-	    PieceType getPieceType() override;
-	    int getPositionalScore(char x, char y) override;
-        std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y) override;
+class Piece
+{
+private:
+    static const char symbol = ' ';
+    static const int value = 0;
+    static const PieceType pieceType = PieceType::None;
+
+public:
+    Color color;
+    virtual char getSymbol();
+    virtual int getValue();
+    virtual PieceType getPieceType();
+    virtual int getPositionalScore(char x, char y);
+    std::vector<std::tuple<char, char>> moveList;
+    virtual std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y);
 };
 
-class Knight : public Piece {
-    private:
-        static const char symbol = 'n';
-        static const int value = 3;
-        static const PieceType pieceType = PieceType::Knight;
-    public:
-        Knight(Color color);
-        char getSymbol() override;
-	    int getValue() override;
-	    PieceType getPieceType() override;
-	    int getPositionalScore(char x, char y) override;
-        std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y) override;
+class Pawn : public Piece
+{
+private:
+    static const char symbol = 'p';
+    static const int value = 1;
+    static const PieceType pieceType = PieceType::Pawn;
+
+public:
+    Pawn(Color color);
+    // char getSymbol() override;
+    // int getValue() override;
+    // PieceType getPieceType() override;
+    int getPositionalScore(char x, char y) override;
+    std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y) override;
 };
 
-class Bishop : public Piece {
-    private:
-        static const char symbol = 'b';
-        static const int value = 3;
-        static const PieceType pieceType = PieceType::Bishop;
-    public:
-        Bishop(Color color);
-        char getSymbol() override;
-	    int getValue() override;
-	    PieceType getPieceType() override;
-	    int getPositionalScore(char x, char y) override;
-        std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y) override;
+class Knight : public Piece
+{
+private:
+    static const char symbol = 'n';
+    static const int value = 3;
+    static const PieceType pieceType = PieceType::Knight;
+
+public:
+    Knight(Color color);
+    // char getSymbol() override;
+    // int getValue() override;
+    // PieceType getPieceType() override;
+    int getPositionalScore(char x, char y) override;
+    std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y) override;
 };
 
-class Rook : public Piece {
-    private:
-        static const char symbol = 'r';
-        static const int value = 5;
-        static const PieceType pieceType = PieceType::Rook;
-    public:
-        Rook(Color color);
-        char getSymbol() override;
-	    int getValue() override;
-	    PieceType getPieceType() override;
-	    int getPositionalScore(char x, char y) override;
-        std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y) override;
+class Bishop : public Piece
+{
+private:
+    static const char symbol = 'b';
+    static const int value = 3;
+    static const PieceType pieceType = PieceType::Bishop;
+
+public:
+    Bishop(Color color);
+    // char getSymbol() override;
+    // int getValue() override;
+    // PieceType getPieceType() override;
+    int getPositionalScore(char x, char y) override;
+    std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y) override;
 };
 
-class Queen : public Piece {
-    private:
-        static const char symbol = 'q';
-        static const int value = 9;
-        static const PieceType pieceType = PieceType::Queen;
-    public:
-        Queen(Color color);
-        char getSymbol() override;
-	    int getValue() override;
-	    PieceType getPieceType() override;
-	    int getPositionalScore(char x, char y) override;
-        std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y) override;
+class Rook : public Piece
+{
+private:
+    static const char symbol = 'r';
+    static const int value = 5;
+    static const PieceType pieceType = PieceType::Rook;
+
+public:
+    Rook(Color color);
+    // char getSymbol() override;
+    // int getValue() override;
+    // PieceType getPieceType() override;
+    int getPositionalScore(char x, char y) override;
+    std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y) override;
 };
 
-class King : public Piece {
-    private:
-        static const char symbol = 'k';
-        static const int value = 900;
-        static const PieceType pieceType = PieceType::King;
-    public:
-        King(Color color);
-        char getSymbol() override;
-	    int getValue() override;
-	    PieceType getPieceType() override;
-	    int getPositionalScore(char x, char y) override;
-        std::vector<std::tuple<char, char>> getMoveList(Piece* squares[8][8], char x, char y) override;
+class Queen : public Piece
+{
+private:
+    static const char symbol = 'q';
+    static const int value = 9;
+    static const PieceType pieceType = PieceType::Queen;
+
+public:
+    Queen(Color color);
+    // char getSymbol() override;
+    // int getValue() override;
+    // PieceType getPieceType() override;
+    int getPositionalScore(char x, char y) override;
+    std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y) override;
+};
+
+class King : public Piece
+{
+private:
+    static const char symbol = 'k';
+    static const int value = 900;
+    static const PieceType pieceType = PieceType::King;
+
+public:
+    King(Color color);
+    // char getSymbol() override;
+    // int getValue() override;
+    // PieceType getPieceType() override;
+    int getPositionalScore(char x, char y) override;
+    std::vector<std::tuple<char, char>> getMoveList(Piece *squares[8][8], char x, char y) override;
 };
